861dd006b89bef9b690186630a4435d4c770fb0b
unknown <lou@sleede.com>
2021-02-18 10:34:30 +0100

    ViewBinding & Two terms addition

--- app/src/main/java/com/batierlou/tp01/MainActivity.java
+++ app/src/main/java/com/batierlou/tp01/MainActivity.java
@@ -4,44 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.batierlou.tp01.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button toastButton;
-    private Button incButton;
-    private TextView counterTextView;
     private Integer counter;
+    private Double firstTerm;
+    private Double secondTerm;
+    private Double result;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         initViews();
     }
 
     private void initViews() {
-        toastButton = findViewById(R.id.toastButton);
-        incButton = findViewById(R.id.incButton);
-        counterTextView = findViewById(R.id.counterTextView);
         counter = 0;
 
-        incButton.setOnClickListener(new View.OnClickListener() {
+        binding.toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, counter.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.incButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
                 counter += 1;
-                counterTextView.setText(counter.toString());
+                binding.counterTextView.setText(counter.toString());
             }
         });
 
-        toastButton.setOnClickListener(new View.OnClickListener() {
+        binding.addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, counter.toString(), Toast.LENGTH_SHORT).show();
+                firstTerm = Double.parseDouble(String.valueOf(binding.firstTerm.getText()));
+                secondTerm = Double.parseDouble(String.valueOf(binding.secondTerm.getText()));
+                result = firstTerm + secondTerm;
+                
+                binding.counterTextView.setText(result.toString());
             }
         });
     }
